770d0e059a2dfe2ba917d72b9a165d16c5270a9e
KarimZakaria <54362523+KarimZakaria@users.noreply.github.com>
2022-10-24 10:47:28 +0200

    Uplaod structure & DB Class

---
+++ controllers/DBController.php
@@ -0,0 +1,26 @@
+<?php
+
+class Db
+{
+    var $servername, $db_username, $db_password, $dbname;
+    // database info => updated later  
+    public function __construct($servername, $db_username, $db_password, $dbname)
+    {
+        $this->servername = $servername;
+        $this->db_username = $db_username;
+        $this->db_password = $db_password;
+        $this->dbname = $dbname;
+    }
+
+    // Make Connection 
+    public function getConnection()
+    {
+        $conn = new mysqli($this->servername, $this->db_username, $this->db_password, $this->dbname);
+        return $conn;
+    }
+}
+
+// Take an Object From Class Db 
+$db = new Db("localhost", "root", "", "php_iti_project");
+// $conn => used in any page you use database to fetch, get, update and delete data. as ( global $conn)
+$conn = $db->getConnection();
---
+++ single_page.php
@@ -0,0 +1,39 @@
+<!-- Start Includes -->
+<?php
+include('includes/session.php');
+include('includes/head.php');
+include('includes/navbar.php');
+?>
+
+<!-- Start Main  -->
+<main id="main">
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Sample Inner Page</h2>
+                <ol>
+                    <li><a href="index.html">Home</a></li>
+                    <li>Sample Inner Page</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumbs -->
+
+    <section class="sample-page">
+        <div class="container" data-aos="fade-up">
+            <p>
+                You can duplicate this sample page and create any number of inner pages
+                you like!
+            </p>
+        </div>
+    </section>
+</main>
+<!-- End #main -->
+
+<!-- Includes Footer and scripts -->
+<?php
+include('includes/footer.php');
+include('includes/scripts.php');
+?>
